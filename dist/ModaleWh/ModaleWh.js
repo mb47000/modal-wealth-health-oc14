213fbf6887f94ce0676ef446fc5bdefd62b2e9aa
Michel Brousse <mbweb47@gmail.com>
2022-04-19 17:16:48 +0200

    Config babel and build command

---
+++ dist/ModaleWh/ModaleWh.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ModaleWh = function ModaleWh() {
+  return /*#__PURE__*/_react.default.createElement("div", null, "hello ModalWh");
+};
+
+var _default = ModaleWh;
+exports.default = _default;
---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ModaleWh", {
+  enumerable: true,
+  get: function get() {
+    return _ModaleWh.default;
+  }
+});
+
+var _ModaleWh = _interopRequireDefault(require("./ModaleWh/ModaleWh"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
